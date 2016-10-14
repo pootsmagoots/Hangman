eea565393caed494f62a0c24835721d39fa4b47b
Travis Batts <Travisbatts6@gmail.com>
2016-10-14 08:57:21 -0400

    project1

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+confirm("Are you ready to play?");
+
+$("button").on("click",function(){
+  alert("Let the game begin!")
+})
+
+var str = prompt("please enter a word!")
+var res = str.split("");
+
+
+
+
+})
